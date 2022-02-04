022dae8253fe7f169f85c3306d7f29ee9e5b1783
Kenramiscal1106 <ramiscalken@gmail.com>
2022-02-04 11:11:48 +0800

    added state management for the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import {ReactComponent as ThemeToggle1} from './icons/Theme-toggle01.svg'
 import {ReactComponent as ThemeToggle2} from './icons/Theme-toggle02.svg'
 
 
 function App() {
+  const [theme, setTheme] = useState('');
+  const [dark, setDark] = useState(false)
+  useEffect(() => {
+    if (dark) setTheme('dark')
+    else setTheme('')
+  }, [dark])
+  const changeTheme = () => {
+    setDark(!dark)
+  }
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
       <h1>This works</h1>
-      <ThemeToggle2 />
-      <ThemeToggle1 />
+      <button className={`theme-toggle ${theme}`} onClick={changeTheme} id="Theme-toggle-radio"><ThemeToggle2 /></button>
+      <button className={`theme-toggle ${theme}`} onClick={changeTheme} id="Theme-toggle-button"><ThemeToggle1 /></button>
+      
     </div>
   );
 }
