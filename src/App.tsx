7f4c9ab087b53452fdee17c1c32a39666c3f220f
Kenramiscal1106 <ramiscalken@gmail.com>
2022-02-15 07:00:14 +0800

    Modified file: - imported test - added a debugger

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import {ReactComponent as ThemeToggle1} from './icons/Theme-toggle01.svg'
 import {ReactComponent as ThemeToggle2} from './icons/Theme-toggle02.svg'
-
+import { test } from './test';
 
 function App() {
   const [theme, setTheme] = useState('');
-  const [dark, setDark] = useState(false)
+  const [dark, setDark] = useState(false);
   useEffect(() => {
     if (dark) setTheme('dark')
     else setTheme('')
+    console.log((test(dark, true) ? "It's dark mode" : "It's light mode"))
   }, [dark])
   const changeTheme = () => {
     setDark(!dark)
